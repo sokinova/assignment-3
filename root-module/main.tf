c1688b4dfc57b384c128192b3582dace0ebba9fd
aziza <aziza.sok@edu.312school.com>
2025-10-13 16:36:56 -0500

    create new root module

---
+++ root-module/main.tf
@@ -0,0 +1,40 @@
+#VPC module
+module "vpc-networking" {
+  source = "../child-modules/vpc-networking"
+  vpc_cidr = var.vpc_cidr
+  vpc_name = var.vpc_name
+  igw_name = var.igw_name
+  rt_name  = var.rt_name
+  region = var.region
+}
+
+#SG-RDS module
+module "rds" {
+  source = "../child-modules/sg-rds"
+  vpc_id = module.vpc-networking.vpc_id
+  private_subnet_ids = module.vpc-networking.private_subnet_ids
+  rds_name = var.rds_name
+  rds_allocated_storage = var.rds_allocated_storage
+  rds_storage_type = var.rds_storage_type
+  rds_engine = var.rds_engine
+  rds_engine_version = var.rds_engine_version
+  rds_instance_class = var.rds_instance_class
+  mysql_user = var.mysql_username
+  mysql_pass = var.mysql_password
+  rds_db_name = var.rds_db_name
+  ingress_ports = var.ingress_ports
+  
+}
+
+module "ec2" {
+    source         = "../child-modules/ec2"
+    vpc_id   = module.vpc-networking.vpc_id
+    subnets = module.vpc-networking.public_subnet_ids
+    ami  = var.ec2_ami
+    ec2_instance_type = var.ec2_instance_type
+    ec2_instance_key  = var.ec2_instance_key
+    ec2_instance_name = var.ec2_instance_name
+    ec2_sg_name       = var.ec2_sg_name
+    ec2_sg_description = var.ec2_sg_description
+    ec2_sg_allowed_ports = var.ec2_sg_allowed_ports
+}
