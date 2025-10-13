f29a566c7d661f20aca31c366bd8b34995d3a1b8
aziza <aziza.sok@edu.312school.com>
2025-10-12 22:16:24 -0500

    add Associate IGW route table with public subnets

--- child modules/vpc-networking/rt.tf
+++ child modules/vpc-networking/rt.tf
@@ -1,5 +1,5 @@
-resource "aws_route_table_association" "rt-igw" {
- vpc_id = aws_vpc.vpc.id
+resource "aws_route_table" "rt-igw" {
+    vpc_id = aws_vpc.vpc.id
     
     route {
     cidr_block = "0.0.0.0/0"
@@ -9,4 +9,20 @@ resource "aws_route_table_association" "rt-igw" {
   tags = {
     Name = var.rt_name
   }
+}
+
+# Associate IGW route table with public subnets
+resource "aws_route_table_association" "public_subnet_1_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.rt-igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.rt-igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_3_assoc" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.rt-igw.id
 }
